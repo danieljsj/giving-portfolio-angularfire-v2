05aae62dd53d7e7d5b351c48993b767d13f7143e
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-07-23 21:14:52 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularfire2App
+ * @description
+ * # angularfire2App
+ *
+ * Main module of the application.
+ */
+angular.module('angularfire2App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
