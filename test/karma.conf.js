e848d7790e477a55b9eb61c0fd619a7bbaa2cb87
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-07-30 15:57:58 -0500

    installed highcharts-release

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,6 +35,9 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/highcharts-release/highcharts.js',
+      'bower_components/highcharts-release/highcharts-more.js',
+      'bower_components/highcharts-release/modules/exporting.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
