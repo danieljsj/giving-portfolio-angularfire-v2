abb2a7ecad36be972467bb3e0acaf0a044705c23
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-03 00:09:00 -0500

    comments on deploy.bash

--- deploy.bash
+++ deploy.bash
@@ -14,9 +14,10 @@ original_branch_name=${branch_name##refs/heads/}
 
 #todo: quit the script if not currently in master
 
-#getting the 'deploy' branch to contain ALL (up-to-date) site files:
+# getting the 'deploy' branch to contain ALL (up-to-date) site files:
 
 git checkout deploy
+read -p "If checking out 'deploy' failed... then hit ctrl+c and go commit everything into current, ostensibly master, branch"
 git merge master
 
 bower install
@@ -26,6 +27,7 @@ sass app/styles/main.scss app/styles/main.css
 
 git add app/bower_components
 git add app/styles/main.css
+# by adding all the newly created files into the repo, it should assure that they're gone and clean from other branches when we check those other branches out
 
 git commit -m "deployment commit of bower_components, main.css"
 echo "Don't worry if there's a commit error; it just means no changes to main.css or bower_components."
@@ -44,4 +46,4 @@ echo
 echo "NOTE: PhoneGapBuild must be built/triggered manually." # Implementation is too large for this script, and requires storing password. not worth it. http://snipplr.com/view/61101/one-click-build-for-phonegap-build-to-android/
 
 
-git checkout $original_branch_name
+git checkout $original_branch_name #generally this should be master, but I don't want to throw people onto master if they started elsewhere.
