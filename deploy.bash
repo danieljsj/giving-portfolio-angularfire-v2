d7e98f935626472a652335f7aee59898f6ece54f
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-02 23:43:26 -0500

    deploy script tweaks

--- deploy.bash
+++ deploy.bash
@@ -14,19 +14,34 @@ original_branch_name=${branch_name##refs/heads/}
 
 #todo: quit the script if not currently in master
 
-git checkout deploy
+#getting the 'deploy' branch to contain ALL (up-to-date) site files:
 
+git checkout deploy
 git merge master
 
 bower install
-
 rsync -r bower_components/ app/bower_components/
 
 sass app/styles/main.scss app/styles/main.css
 
-firebase deploy
+git add app/bower_components
+git add app/styles/main.css
 
-#todo: trigger a phonegap update programmatically
+git commit -m "deployment commit of bower_components, main.css"
+echo "Don't worry if there's a commit error; it just means no changes to main.css or bower_components."
+git push origin deploy
+echo
+
+
+read -p "firebase deploy? (y/n): " #-n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    firebase deploy
+fi
+
+echo
+echo "NOTE: PhoneGapBuild must be built/triggered manually." # Implementation is too large for this script, and requires storing password. not worth it. http://snipplr.com/view/61101/one-click-build-for-phonegap-build-to-android/
 
 
 git checkout $original_branch_name
