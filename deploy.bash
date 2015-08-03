cbf894ea02606c944e10391dfdbb62883bcad387
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2015-08-02 23:34:12 -0500

    added deployment bash script

--- deploy.bash
+++ deploy.bash
@@ -1,14 +1,32 @@
 #!/bin/bash
-
+echo DEPLOMENT ACTIONS:
+cat deploy.bash
 echo
-echo "CREATING MAIN STYLESHEET"
-echo "via: sass app/styles/main.scss app/styles/main.css"
+echo
+read -p "READ AND PRESS ANY KEY TO APPROVE, OR PRESS CTRL+C TO CANCEL!"
+
+
+
+original_branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+original_branch_name="(unnamed branch)"     # detached HEAD
+original_branch_name=${branch_name##refs/heads/}
+
+
+#todo: quit the script if not currently in master
+
+git checkout deploy
+
+git merge master
+
+bower install
+
+rsync -r bower_components/ app/bower_components/
 
 sass app/styles/main.scss app/styles/main.css
 
-
-echo
-echo "DEPLOYING TO FIREBASE"
-echo "via 'firebase deploy' :"
-
 firebase deploy
+
+#todo: trigger a phonegap update programmatically
+
+
+git checkout $original_branch_name
